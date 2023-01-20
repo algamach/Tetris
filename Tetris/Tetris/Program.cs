19e70b96aff4d522177c1bba445df1a149f470e6
algamach <123144931+algamach@users.noreply.github.com>
2023-01-20 10:33:18 +0300

    Add class Point

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Tetris!!!");
+﻿namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            Point p1 = new Point();
+            p1.x = 12;
+            p1.y = 12;
+            p1.c = '#';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 2;
+            p2.y = 7;
+            p2.c = '#';
+            p2.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
