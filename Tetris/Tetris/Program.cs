dd48dde780912a1d73bd24847c6c42e2c1fababe
algamach <123144931+algamach@users.noreply.github.com>
2023-06-22 13:54:18 +0300

    Timer, Monitor
    Race condition

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -1,59 +1,88 @@
-﻿namespace Tetris
+﻿using System.Threading;
+using System.Timers;
+
+namespace Tetris
 {
-    class Program
-    {
-        static FigureGenerator generator;
-        static void Main(string[] args)
+	class Program
+	{
+		const int TIMER_INTERVAL = 500;
+		static System.Timers.Timer timer;
+		static private Object _lockObject = new Object();
+	
+		static Figure currentFigure;
+		static FigureGenerator generator;
+
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(Field.Width, Field.Height);
+			Console.SetBufferSize(Field.Width, Field.Height);
+
+			generator = new FigureGenerator(Field.Width / 2, 0, Drawer.DEFAUTL_SYMBOL);
+			currentFigure = generator.GetNewFigure();
+			SetTimer();
+
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					var key = Console.ReadKey();
+					Monitor.Enter(_lockObject);
+					var result = HandleKey(currentFigure, key.Key);
+					ProcessResult(result, ref currentFigure);
+					Monitor.Exit(_lockObject);
+				}
+			}
+		}
+
+		private static bool ProcessResult(Result result, ref Figure currentFigure)
+		{
+			if (result == Result.HEAP_STRIKE || result == Result.DOWN_BORDER_STRIKE)
+			{
+				Field.AddFigure(currentFigure);
+				Field.TryDeleteLines();
+				currentFigure = generator.GetNewFigure();
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		private static Result HandleKey(Figure f, ConsoleKey key)
+		{
+			switch (key)
+			{
+				case ConsoleKey.LeftArrow:
+					return f.TryMove(Direction.LEFT);
+				case ConsoleKey.RightArrow:
+					return f.TryMove(Direction.RIGHT);
+				case ConsoleKey.DownArrow:
+					return f.TryMove(Direction.DOWN);
+				//UP will need to be removed
+				case ConsoleKey.UpArrow:
+					return f.TryMove(Direction.UP);
+				case ConsoleKey.Spacebar:
+					return f.TryRotate();
+			}
+			return Result.SUCCESS;
+		}
+		private static void SetTimer()
         {
-            Console.SetWindowSize(Field.Width, Field.Height);
-            Console.SetBufferSize(Field.Width, Field.Height);
-
-            generator = new FigureGenerator(Field.Width / 2, 0, Drawer.DEFAUTL_SYMBOL);
-            Figure currentFigure = generator.GetNewFigure();
-
-            while (true)
-            {
-                if (Console.KeyAvailable)
-                {
-                    var key = Console.ReadKey();
-                    var result = HandleKey(currentFigure, key.Key);
-                    ProcessResult(result, ref currentFigure);
-                }
-            }
+            // Create a timer with a two second interval.
+            timer = new System.Timers.Timer(TIMER_INTERVAL);
+            // Hook up the Elapsed event for the timer. 
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
         }
 
-        private static bool ProcessResult(Result result, ref Figure currentFigure)
+        private static void OnTimedEvent(object? sender, ElapsedEventArgs e)
         {
-            if (result == Result.HEAP_STRIKE || result == Result.DOWN_BORDER_STRIKE)
-            {
-                Field.AddFigure(currentFigure);
-                Field.TryDeleteLines();
-                currentFigure = generator.GetNewFigure();
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private static Result HandleKey(Figure f, ConsoleKey key)
-        {
-            switch (key)
-            {
-                case ConsoleKey.LeftArrow:
-                    return f.TryMove(Direction.LEFT);
-                case ConsoleKey.RightArrow:
-                    return f.TryMove(Direction.RIGHT);
-                case ConsoleKey.DownArrow:
-                    return f.TryMove(Direction.DOWN);
-                //UP will need to be removed
-                case ConsoleKey.UpArrow:
-                    return f.TryMove(Direction.UP);
-                case ConsoleKey.Spacebar:
-                    return f.TryRotate();
-            }
-            return Result.SUCCESS;
+            Monitor.Enter(_lockObject);
+            var result = currentFigure.TryMove(Direction.DOWN);
+			ProcessResult(result, ref currentFigure);
+            Monitor.Exit(_lockObject);
         }
     }
 }
